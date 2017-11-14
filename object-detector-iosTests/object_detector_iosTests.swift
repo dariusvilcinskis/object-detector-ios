01c54754efd17bf37c2bf4e528d057b00075533e
Darius <darius.vilcinskis@live.com>
2017-11-14 16:53:11 +0200

    Initial Commit

---
+++ object-detector-iosTests/object_detector_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  object_detector_iosTests.swift
+//  object-detector-iosTests
+//
+//  Created by Darius Vilcinskis on 14/11/2017.
+//  Copyright © 2017 Darius Vilcinskis. All rights reserved.
+//
+
+import XCTest
+@testable import object_detector_ios
+
+class object_detector_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
